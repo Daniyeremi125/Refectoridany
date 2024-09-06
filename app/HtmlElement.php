ea1d9f3a1f89e2fe30a7a650580229c2b3673dc4
Daniyeremi12345 <202100352G@utea.edu.pe>
2024-09-05 20:25:12 -0500

    contenido

--- app/HtmlElement.php
+++ app/HtmlElement.php
@@ -1,25 +1,53 @@
 <?php  
 namespace App;  
 
-class HtmlElement {  
-    
-    private $name;
 
-public function __construct(string $name)
-{
-    $this->name =$name;
-}
-    
-public function render()
+class HtmlElement 
+{  
+private $name,$content,$attributes;
 
- {
-    
-   // abrir la etiqueta
-   $result ='<'.$this->name.'>';
 
-    //CERRAR LA ETIQUETA
+    public function __construct(string $name,array $attributes=[], $content = null)
+    {
+        $this->attributes =$attributes;
+        $this->name =$name;
+        $this->content=$content;
+        
+    }
 
-    return $result;
- }
+    public function render()
+    {
 
+        if (! empty($this->attributes)){
+
+            $htmlAttributes='';
+
+            foreach($this->attributes as $attributes => $value){
+
+                if(is_numeric($attributes)){
+                    $htmlAttributes .=' '.$value;
+                } else{
+                    $htmlAttributes .= ' '. $attributes.'="'.htmlentities($value,ENT_QUOTES, 'UTF-8').'"';
+                }
+
+            }
+
+            $result ='<'.$this->name.$htmlAttributes.   '>';
+        }else {
+            $result ='<'.$this->name.'>';
+        }
+       
+       if (in_array($this->name,['br','hr','img','input','meta']) ){
+        return $result;
+       }
+
+
+
+
+        $result.= htmlentities($this->content,ENT_QUOTES, 'UTF-8');
+     
+
+        $result .= '</'.$this->name.'>';
+        return $result;
+    }
 }
