c3bbc0e99ee3dea27e92e4f63b2f04d6cbf737c3
Daniyeremi12345 <202100352G@utea.edu.pe>
2024-09-05 20:25:29 -0500

    contenido

--- public/index.php
+++ public/index.php
@@ -1,7 +1,39 @@
 <?php  
 
-require '../vendor/autoload.php';  
+    require '../vendor/autoload.php';  
 
-$element = new \App\HtmlElement('p');  
+    use App\HtmlElement;  
 
-echo htmlentities($element->render(),ENT_QUOTES, 'UTF-8') ;
+    $element = new \App\HtmlElement('p',[],'este es el contemido');  
+
+    echo htmlentities( $element->render(), ENT_QUOTES, 'UTF-8');
+
+    echo '<br><br>';
+
+    $element = new \App\HtmlElement('p',['id'=>'my_paragraph'],'este es el contemido');  
+
+    echo htmlentities( $element->render(), ENT_QUOTES, 'UTF-8');
+
+    echo '<br><br>';
+
+    $element = new \App\HtmlElement('p',['id'=>'my_paragraph', 'class' => 'paragraph'],'este es el contemido');  
+
+    echo htmlentities( $element->render(), ENT_QUOTES, 'UTF-8');
+
+    echo '<br><br>';
+
+    $element = new \App\HtmlElement('img',['src'=>'img/styde.png']);  
+
+    echo htmlentities( $element->render(), ENT_QUOTES, 'UTF-8');
+    
+    echo '<br><br>';
+
+    $element = new \App\HtmlElement('img',['src'=>'img/styde.png','title'=>'Curso de "refactorizacion" en styde']);  
+
+    echo htmlentities( $element->render(), ENT_QUOTES, 'UTF-8');
+
+    echo '<br><br>';
+
+    $element = new \App\HtmlElement('input',['required']);  
+
+    echo htmlentities( $element->render(), ENT_QUOTES, 'UTF-8');
